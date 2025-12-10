cf8ee922f49fb08a8cccf2cd12415a3ed5479243
vmq-16 <quangvm16@gmail.com>
2025-12-10 12:58:14 +0700

    Update Renderer.h

--- Renderer.h
+++ Renderer.h
@@ -15,13 +15,22 @@
 using namespace Gdiplus;
 
 class Renderer {
+
 public:
     Renderer() = default;
     ~Renderer() = default;
 
     void drawShape(Graphics& g, const SVG::Shape* shape) const;
-    
     void renderAll(Graphics& g, const std::vector<SVG::Shape*>& shapes) const;
+
+private:
+    void drawLine(Graphics& g, const SVG::Line* line) const;
+    void drawRect(Graphics& g, const SVG::Rect* rect) const;
+    void drawCircle(Graphics& g, const SVG::Circle* circle) const;
+    void drawEllipse(Graphics& g, const SVG::Ellipse* ellipse) const;
+    void drawPolyline(Graphics& g, const SVG::Polyline* poly) const;
+    void drawPolygon(Graphics& g, const SVG::Polygon* poly) const;
+    void drawText(Graphics& g, const SVG::Text* text) const;
 };
 
 #endif // RENDERER_H
