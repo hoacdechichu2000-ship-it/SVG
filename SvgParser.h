e8c1960d549b88ec227512548b5490e5898b2e26
vmq-16 <quangvm16@gmail.com>
2025-12-10 13:00:20 +0700

    Update SvgParser.h

--- SvgParser.h
+++ SvgParser.h
@@ -1,23 +1,15 @@
 #ifndef SVGPARSER_H
 #define SVGPARSER_H
 
+#include "Style.h"
 #include "Shape.h"
 #include <vector>
 #include <string>
 
-struct Style {
-    std::string fill, stroke;
-    double fillOpacity, strokeOpacity, strokeWidth;
-};
-
 class SVGParser {
 private:
-    std::vector<SVG::Shape*> shapes;    // lưu tất cả Shape
-
-public:
-    // Constructor & Destructor
-    SVGParser() = default;
-    ~SVGParser();
+    // lưu tất cả Shape
+    std::vector<SVG::Shape*> shapes;    
 
     // Lấy các thông số string, int, double
     std::string getAttrString(const std::string& line, const std::string& attr) const;
@@ -27,12 +19,6 @@ public:
     // Lấy thuộc tính points
     std::vector<std::pair<int,int>> getPoints(const std::string& pointsStr) const;
 
-    // Lấy các thuộc tính màu sắc
-    Style getStyle(const std::string& line) const;
-
-    // Đọc file SVG
-    void parserFile(const std::string& filename);
-
     // Parse từng loại Shape
     void parserRect(const std::string& line);
     void parserCircle(const std::string& line);
@@ -41,6 +27,17 @@ public:
     void parserLine(const std::string& line);
     void parserPolyline(const std::string& line);
     void parserPolygon(const std::string& line);
+
+    // Lấy các thuộc tính màu sắc
+    SVG::Style parserStyle(const std::string& line) const;
+
+public:
+    // Constructor & Destructor
+    SVGParser() = default;
+    ~SVGParser();
+
+    // Đọc file SVG
+    void parserFile(const std::string& filename);
 
     // Getter
     const std::vector<SVG::Shape*>& getShapes() const { return shapes; }
