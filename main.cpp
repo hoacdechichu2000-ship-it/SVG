a14eddf1c4afd5738fdafa0796f8ba1d672180e1
vmq-16 <quangvm16@gmail.com>
2025-12-14 16:07:13 +0700

    Remove duplicate code

--- main.cpp
+++ main.cpp
@@ -14,43 +14,6 @@ int WINAPI wWinMain(
 
     AppWindow window;
 
-    HWND hwnd = window.Create(L"SVG Viewer", hInstance);
-
-    if (hwnd == NULL)
-    {
-        return 0;
-    }
-
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
-
-    MSG msg = { };
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return (int)msg.wParam;
-
-}
-
-#include "AppWindow.h"
-
-int WINAPI wWinMain(
-    _In_ HINSTANCE hInstance,
-    _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPWSTR    lpCmdLine,
-    _In_ int       nCmdShow
-)
-{
-    if (!AppWindow::RegisterWindowClass(hInstance))
-    {
-        return 0;
-    }
-
-    AppWindow window;
-
     HWND hwnd = window.Create(L"SVG Viewer ([O]: mo file).", hInstance);
 
     if (hwnd == NULL)
